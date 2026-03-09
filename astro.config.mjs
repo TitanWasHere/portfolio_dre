48a4dba0ad4438edb6cc661b93509f7862f5e7c6
TitanWasHere <andrea.gravili8@gmail.com>
2026-03-09 22:00:58 +0100

    Add GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://TitanWasHere.github.io',
+  base: '/portfolio_dre',
   vite: {
     plugins: [tailwindcss()]
   }
